44e72388f9536f4e4b4c13170b1c5743d0de53c4
Vijay <vijayganganapalli@gmail.com>
2018-09-28 18:32:45 +0530

    initial commit

---
+++ project01/lib/main.dart
@@ -0,0 +1,13 @@
+//TODO: import library
+import 'package:flutter/material.dart';
+
+//TODO: create a main Function
+void main() {
+  //TODO: create a widget
+  var app = MaterialApp(
+    home: Text("Hi there, Vijay from LRP"),
+  );
+
+  //TODO: run that widget on mobile device
+  runApp(app);
+}
