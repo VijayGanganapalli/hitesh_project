50cc29f1ed4879d4eb09f2f95a61978d78d7bb08
Vijay <vijayganganapalli@gmail.com>
2018-09-29 23:48:45 +0530

    center the text

--- project01/lib/main.dart
+++ project01/lib/main.dart
@@ -2,16 +2,16 @@
 import 'package:flutter/material.dart';
 
 //TODO: create a main Function
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-  class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: "Flutter tutorial",
-        home: Text("Hello Flutter!"),
-      );
-    }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter tutorial",
+      home: Center(
+        child: Text("Hello world!"),
+      ),
+    );
   }
+}
