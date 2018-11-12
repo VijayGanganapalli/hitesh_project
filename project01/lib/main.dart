3be7582029333bf3cc3634fa8889d27cc13a2094
Vijay <vijayganganapalli@gmail.com>
2018-11-12 05:57:43 +0530

    icon widget

--- project01/lib/main.dart
+++ project01/lib/main.dart
@@ -1,16 +1,29 @@
-//TODO: import library
 import 'package:flutter/material.dart';
 
-//TODO: create a main Function
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final barColor = const Color(0xFF009688);
+  final bgColor = const Color(0xFFE0F2F1);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter tutorial",
-      home: Center(
-        child: Text("Hello world!"),
+      title: "Instagram",
+      home: Scaffold(
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          title: Text("Instagram"),
+          backgroundColor: barColor,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: barColor,
+          child: Icon(
+            Icons.add,
+            size: 26.0,
+          ),
+          onPressed: () {},
+        ),
       ),
     );
   }
