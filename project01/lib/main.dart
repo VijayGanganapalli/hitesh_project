7b270ff8af66795a96e9a2019923a68288712a5f
Vijay <vijayganganapalli@gmail.com>
2018-11-12 06:15:03 +0530

    reformat the code

--- project01/lib/main.dart
+++ project01/lib/main.dart
@@ -1,30 +1,4 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  final barColor = const Color(0xFF009688);
-  final bgColor = const Color(0xFFE0F2F1);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Instagram",
-      home: Scaffold(
-        backgroundColor: bgColor,
-        appBar: AppBar(
-          title: Text("Instagram"),
-          backgroundColor: barColor,
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: barColor,
-          child: Icon(
-            Icons.add,
-            size: 26.0,
-          ),
-          onPressed: () {},
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(App());
---
+++ project01/lib/src/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  final barColor = const Color(0xFF009688);
+  final bgColor = const Color(0xFFE0F2F1);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Instagram",
+      home: Scaffold(
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          title: Text("Instagram"),
+          backgroundColor: barColor,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: barColor,
+          child: Icon(
+            Icons.add,
+            size: 26.0,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
